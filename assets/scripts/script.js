6ad687c5a29dcb31642b2dae9d04436541ca6202
Edward Huang <ejhuang.2015@gmail.com>
2021-02-14 02:29:24 -0500

    Added second dropdown menu in navbar and added closing menu when clicking outside of menu functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,40 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Navbar functionality
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    // Opens/Closes nav menu on click of hamburger menu
+    $(".navbar-burger").click(function () {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
-
-    $(".navLink").click(function() {
-        console.log(this);
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
+    // Closes all dropdown menus when a nav menu link is clicked
+    $(".navLink").click(function () {
+        $(".navbar-burger").removeClass("is-active");
+        $(".navbar-menu").removeClass("is-active");
+        $(".has-dropdown").removeClass("is-active");
         $(".navbar-dropdown").addClass("invis");
     });
-
-    $(".navbar-link").click(function() {
-        $(".has-dropdown").toggleClass("is-active");
-        $(".navbar-dropdown").toggleClass("invis");
+    // Opens/Closes dropdown menu on click of dropdown title
+    $("#navPortfolio").click(function () {
+        $("#navPortfolioHasDropdown").toggleClass("is-active");
+        $("#navPortfolioDropdown").toggleClass("invis");
+    });
+    $("#navContacts").click(function () {
+        $("#navContactsHasDropdown").toggleClass("is-active");
+        $("#navContactsDropdown").toggleClass("invis");
+    });
+    // Closes all nav menus when clicking...
+    $(document).click(function () {
+        $(".navbar-burger").removeClass("is-active");
+        $(".navbar-menu").removeClass("is-active");
+        $(".has-dropdown").removeClass("is-active");
+        $(".navbar-dropdown").addClass("invis");
+    });
+    // ...except when clicking the mobile nav-bar or the dropdown menus
+    $(".navbar-brand").click(function (e) {
+        e.stopPropagation();
+    });
+    $(".navbar-menu").click(function (e) {
+        e.stopPropagation();
     });
 
-  });
+});
