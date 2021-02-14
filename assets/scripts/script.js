0ce46af73ba732c21c811724041b69ad1f166ba9
Edward Huang <ejhuang.2015@gmail.com>
2021-02-14 00:58:18 -0500

    Fussed around with the navbar dropdowns and css

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,5 +7,17 @@ $(document).ready(function() {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
-    
+
+    $(".navLink").click(function() {
+        console.log(this);
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+        $(".navbar-dropdown").addClass("invis");
+    });
+
+    $(".navbar-link").click(function() {
+        $(".has-dropdown").toggleClass("is-active");
+        $(".navbar-dropdown").toggleClass("invis");
+    });
+
   });
